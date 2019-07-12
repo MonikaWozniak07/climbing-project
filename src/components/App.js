4c023419b43c1317106906ff0faf4ae69089641f
Monika Woźniak <monikawozniak07@gmail.com>
2019-07-12 13:44:56 +0200

    modyfikacja price

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className="wrapper">
+        <div className="wrapperek">
           <header>
             <Header />
           </header>
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,62 +1,77 @@
 import React from "react";
 import "../styles/Store.css";
 import img1 from "../img/shoes.jpg";
-// import img2 from "../img/buty.jpg";
 import img3 from "../img/hamak.jpg";
 import img4 from "../img/kask.jpg";
 import img5 from "../img/przyrzad-as.jpg";
-// import img6 from "../img/karabinek.jpg";
 import img7 from "../img/rope.jpg";
 import img8 from "../img/uprząż.jpg";
 class Store extends React.Component {
-  state = {};
+  state = {
+    buty: "199 zł",
+    hamak: "79 zł",
+    kask: "109 zł",
+    prz: "49 zł",
+    lina: "389 zł",
+    uprząż: "189 zł",
+    price: null
+  };
+
   render() {
+    const { buty, hamak, kask, prz, lina, uprząż, price } = this.state;
+
     return (
       <>
+        <div className="koszyk">
+          Do zapłaty:
+          <input value={price} />
+          <button>Płacę</button>
+        </div>
+        <span>
+          Zachęcamy do zakupów w naszym sklepie. Do wyboru mają państwo produkty
+          najwyższej klasy. Najważniejszy dla nas jest komfort klienta.
+        </span>
         <div className="wrapper">
           <div className="products">
             <img src={img1} alt="buty wspinaczkowe" />
-            <h2>Buty wspinaczkowe</h2>
+            <h2>Buty wspinaczkowe {buty}</h2>
             <p>
               Niezbętnik dla wszystkich wspinaczy. Zapewniają komfortową
               wspinaczkę.{" "}
             </p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
-          {/* <div className="products">
-            <img src={img2} alt="buty górskie" />
-            <h2>Buty górskie</h2>
-            <p>Buty idealne na długie, górskie wędrówki.</p>
-          </div> */}
           <div className="products">
             <img src={img3} alt="hamak" />
-            <h2>Hamak</h2>
+            <h2>Hamak {hamak}</h2>
             <p>Superwygodny i wytrzymały. Nigdzie kawa nie smakuje lepiej ;)</p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
           <div className="products">
             <img src={img4} alt="kask" />
-            <h2>Kask</h2>
+            <h2>Kask {kask}</h2>
             <p>Dbajmy o swoje bezpieczeństwo. Wspinajmy się w kasku!</p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
           <div className="products">
             <img src={img5} alt="przyrząd asekuracyjny" />
-            <h2>Przyrząd asekuracyjny</h2>
+            <h2>Przyrząd asekuracyjny {prz}</h2>
             <p>Niezawodny przyrząd asekuracyjny.</p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
-          {/* <div className="products">
-            <img src={img6} alt="karabinek" /> <h2>Karabinek</h2>
-            <p>Wytrzymały, łatwy w obsłudze.</p>
-          </div> */}
           <div className="products">
             <img src={img7} alt="lina" />
-            <h2>Lina</h2>
+            <h2>Lina {lina}</h2>
             <p>
               Lina statyczna, doskonała do wspinaczki oraz pracy na wysokości.
             </p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
           <div className="products">
             <img src={img8} alt="uprząż" />
-            <h2>Uprząż</h2>
+            <h2>Uprząż {uprząż}</h2>
             <p>Wygodna i wytrzymała.</p>
+            <button onClick={this.handleAddProduct}>Dodaj do koszyka</button>
           </div>
         </div>
       </>
