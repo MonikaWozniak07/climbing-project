173e02d236f94d20cca2b5b3b7f437680a02383d
Monika Woźniak <monikawozniak07@gmail.com>
2019-08-14 13:35:52 +0200

    media

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -8,17 +8,31 @@ import img7 from "../img/rope.jpg";
 import img8 from "../img/uprząż.jpg";
 class Store extends React.Component {
   state = {
-    buty: "199 zł",
-    hamak: "79 zł",
-    kask: "109 zł",
-    prz: "49 zł",
-    lina: "389 zł",
-    uprząż: "189 zł",
+    buty: 199,
+    hamak: 79,
+    kask: 109,
+    prz: 49,
+    lina: 389,
+    uprząż: 189,
     price: "0"
   };
 
   render() {
-    const { buty, hamak, kask, prz, lina, uprząż, price } = this.state;
+    const {
+      buty,
+      hamak,
+      kask,
+      prz,
+      lina,
+      uprząż,
+      price
+      // active,
+      // value
+    } = this.state;
+
+    this.handleSubmit = e => {
+      e.preventDefault();
+    };
 
     this.handleRemove = () => {
       this.setState({
@@ -29,8 +43,9 @@ class Store extends React.Component {
     return (
       <>
         <div className="koszyk">
-          Do zapłaty:
-          <input value={price} />
+          <form onSubmit={this.handleSubmit}>
+            <input value={`Do zapłaty: ${price} zł`} />
+          </form>
           <button onClick={this.handleRemove}>Płacę</button>
         </div>
         <span>
@@ -40,92 +55,110 @@ class Store extends React.Component {
         <div className="wrapper">
           <div className="products">
             <img src={img1} alt="buty wspinaczkowe" />
-            <h2>Buty wspinaczkowe {buty}</h2>
+            <h2>Buty wspinaczkowe 199 zł</h2>
             <p>
               Niezbętnik dla wszystkich wspinaczy. Zapewniają komfortową
               wspinaczkę.{" "}
             </p>
             <button
-              onClick={() =>
+              onClick={val =>
                 this.setState({
-                  price: buty
+                  price: parseInt(price) + parseInt(buty)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {price === buty && (
+              <button onClick={this.handleRemoveShoes}> - </button>
+            )} */}
           </div>
           <div className="products">
             <img src={img3} alt="hamak" />
-            <h2>Hamak {hamak}</h2>
+            <h2>Hamak 79 zł</h2>
             <p>Superwygodny i wytrzymały. Nigdzie kawa nie smakuje lepiej ;)</p>
             <button
               onClick={() =>
                 this.setState({
-                  price: hamak
+                  price: parseInt(price) + parseInt(hamak)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {price === hamak && (
+              <button onClick={this.handleRemove}> - </button>
+            )} */}
           </div>
           <div className="products">
             <img src={img4} alt="kask" />
-            <h2>Kask {kask}</h2>
+            <h2>Kask 109 zł</h2>
             <p>Dbajmy o swoje bezpieczeństwo. Wspinajmy się w kasku!</p>
             <button
               onClick={() =>
                 this.setState({
-                  price: kask
+                  price: parseInt(price) + parseInt(kask)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {active === true && (
+              <button onClick={this.handleRemove}> - </button>
+            )} */}
           </div>
           <div className="products">
             <img src={img5} alt="przyrząd asekuracyjny" />
-            <h2>Przyrząd asekuracyjny {prz}</h2>
+            <h2>Przyrząd asekuracyjny 49 zł</h2>
             <p>Niezawodny przyrząd asekuracyjny.</p>
             <button
               onClick={() =>
                 this.setState({
-                  price: prz
+                  price: parseInt(price) + parseInt(prz)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {active === true && (
+              <button onClick={this.handleRemove}> - </button>
+            )} */}
           </div>
           <div className="products">
             <img src={img7} alt="lina" />
-            <h2>Lina {lina}</h2>
+            <h2>Lina 389 zł</h2>
             <p>
               Lina statyczna, doskonała do wspinaczki oraz pracy na wysokości.
             </p>
             <button
               onClick={() =>
                 this.setState({
-                  price: lina
+                  price: parseInt(price) + parseInt(lina)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {active === true && (
+              <button onClick={this.handleRemove}> - </button>
+            )} */}
           </div>
           <div className="products">
             <img src={img8} alt="uprząż" />
-            <h2>Uprząż {uprząż}</h2>
+            <h2>Uprząż 189 zł</h2>
             <p>Wygodna i wytrzymała.</p>
             <button
               onClick={() =>
                 this.setState({
-                  price: uprząż
+                  price: parseInt(price) + parseInt(uprząż)
                 })
               }
             >
               Dodaj do koszyka
-            </button>
+            </button>{" "}
+            {/* {active === true && (
+              <button onClick={this.handleRemove}> - </button>
+            )} */}
           </div>
         </div>
       </>
