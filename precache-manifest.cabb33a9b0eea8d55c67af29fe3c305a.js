2dc01bf252e20e6db7139665594b6117eb8f6da8
Monika Woźniak <monikawozniak07@gmail.com>
2020-09-06 20:41:18 +0200

    Updates

---
+++ precache-manifest.cabb33a9b0eea8d55c67af29fe3c305a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb5e2326f51cc0f4aa4389606f461e0",
+    "url": "/climbing-project/index.html"
+  },
+  {
+    "revision": "3bfb8d3c64656b2680cb",
+    "url": "/climbing-project/static/css/main.03829e5c.chunk.css"
+  },
+  {
+    "revision": "051576479a92aafbdc06",
+    "url": "/climbing-project/static/js/2.a120e3a7.chunk.js"
+  },
+  {
+    "revision": "3bfb8d3c64656b2680cb",
+    "url": "/climbing-project/static/js/main.ffab5919.chunk.js"
+  },
+  {
+    "revision": "83c5cd5858a8ec5349ca",
+    "url": "/climbing-project/static/js/runtime~main.b98f17a2.js"
+  },
+  {
+    "revision": "0c206063281a397d381289676e95b13f",
+    "url": "/climbing-project/static/media/Rock Climbing - 925.0c206063.mp4"
+  },
+  {
+    "revision": "aed53ef34364ac03e7415824206da231",
+    "url": "/climbing-project/static/media/hamak.aed53ef3.jpg"
+  },
+  {
+    "revision": "6f31f3dadb2e8d8465423e128ad93320",
+    "url": "/climbing-project/static/media/kask.6f31f3da.jpg"
+  },
+  {
+    "revision": "d53974302c1e904d5896436597f80d83",
+    "url": "/climbing-project/static/media/logo_transparent.d5397430.png"
+  },
+  {
+    "revision": "0db473e6efaaa49777484fb51f15131c",
+    "url": "/climbing-project/static/media/przyrzad-as.0db473e6.jpg"
+  },
+  {
+    "revision": "dd658db1fdac2d62603c2323fad567a6",
+    "url": "/climbing-project/static/media/rope.dd658db1.jpg"
+  },
+  {
+    "revision": "92b7953a8c69e71ae945ecbe3c995865",
+    "url": "/climbing-project/static/media/shoes.92b7953a.jpg"
+  },
+  {
+    "revision": "ef1bf2b87accd7dc8ce8c528d0ae3525",
+    "url": "/climbing-project/static/media/uprząż.ef1bf2b8.jpg"
+  }
+]);
